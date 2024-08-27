7b71692fa2e216afb6dbda55fdf62f99d42e03d3
Bruno <brunogmr07@gmail.com>
2024-08-27 15:37:00 -0300

    aplicando redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import carrinhoReducer from './reducers/carrinho'
 
+import api from '../services/api'
+
 export const store = configureStore({
   reducer: {
-    carrinho: carrinhoReducer
-  }
+    carrinho: carrinhoReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddlewere) =>
+    getDefaultMiddlewere().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
